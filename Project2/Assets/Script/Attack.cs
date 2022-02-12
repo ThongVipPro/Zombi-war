e5a26bddd531d7a4cecf3dd8db23439dae02f635
DanielChendyrov <linhhonbaothu45@gmail.com>
2022-02-13 02:34:20 +0700

    Added movement constraint and fixing some variables.

--- Project2/Assets/Script/Attack.cs
+++ Project2/Assets/Script/Attack.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-    public Animator anim;
+    [SerializeField] Animator anim;
 
     /*public GameObject arrowPrefab1;*/
     /*[SerializeField] private Transform arrowPoint;*/
@@ -17,6 +17,7 @@ public class Attack : MonoBehaviour
         {
             Attack2();
         }
+
     }
     void Attack2()
     {
@@ -32,7 +33,7 @@ public class Attack : MonoBehaviour
             GameObject arrow = Instantiate(arrowPrefab1, transform.position, Quaternion.identity);
             arrow.GetComponent<Rigidbody2D>().velocity = new Vector2(1.0f, 0.0f);
         }*/
-        
+
         /*GameObject gameObject = GameObject.Find("arrow");
         gameObject.transform.position = arrowPoint.position;
         gameObject.GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));*/
